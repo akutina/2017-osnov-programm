c257b1f1d47c1dea6764340ad8108879a6d8ba0b
Natasha Akutina <akutinabr12@gmail.com>
2017-12-18 20:31:36 +0300

    Add files via upload

--- homework/tokenizer.py
+++ homework/tokenizer.py
@@ -6,8 +6,8 @@ tokens = []
 for c in sys.stdin.readlines(): #I read every line (sentence) in the text
     sent = [] #here is my array for each sentence
     index = 1 #here is index which is 1 at the first step
-    if c != '/n': #if the sentence is not empty
-        for i in c: #for each word in sentence
+    if c != '\n': #if the sentence is not empty
+        for i in c(' '): #for each word in sentence
             if i[-1] not in punct: #if its last symbol is not in punct
                 sent.append([index, i]) # I append i with its index to sent and change index for the next token
                 index+=1
